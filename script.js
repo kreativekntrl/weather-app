73dd20e6e0a90be5068214a756d28085d85fb10c
Ava <avamsteimle@gmail.com>
2022-01-11 09:55:30 -0800

    logged Api data to console, working green now

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
-var apiUrl = "https://api.openweathermap.org/data/2.5/weather?&appid={8df1b4f50fc18065282d76ad07ce45c3}"
+var apiKey = "8df1b4f50fc18065282d76ad07ce45c3"
+var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=san diego&appid=8df1b4f50fc18065282d76ad07ce45c3"
 fetch(apiUrl)
-    .then(function (response) {
-        console.log(response);
-    });
-    
+    .then(function (response){
+        if(response.ok){
+            return response.json();
+        }
+    })
+    .then(function (data){
+        console.log(data);
+    })
